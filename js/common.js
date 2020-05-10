98792a5880b56569fb1f6a4c424008c502ee9cd9
Yaroslav Paltsun <trancemanjake@gmail.com>
2020-05-10 17:42:31 +0300

    some add

--- js/common.js
+++ js/common.js
@@ -21,14 +21,6 @@ $(document).ready(function() {
     });
 
 
-    $('[data-countdown]').each(function() {
-		  var $this = $(this), finalDate = $(this).data('countdown');
-		  $this.countdown(finalDate, function(event) {
-		  	$this.html(event.strftime('%D:%H:%M:%S'));
-		  });
-	});
-
-
 
 
 });
